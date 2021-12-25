1f004164a650078f0594216ee6b1681e2371da8b
jacobways <jacobkjy0501@gmail.com>
2021-12-25 23:22:27 +0900

    chart 날짜 선택 기능 추가

--- server/controllers/chartData/exchangeRate.js
+++ server/controllers/chartData/exchangeRate.js
@@ -14,7 +14,8 @@ module.exports = (req, res) => {
     }
   })
   .then((data)=>{
-    res.status(200).json({message: 'ok', data: data.map((el)=>el.dataValues)})
+    // 필요한 데이터만 정리해서 날짜순으로 정렬
+    res.status(200).json({message: 'ok', data: data.map((el)=>el.dataValues).sort((a,b)=>a.date - b.date)})
   })
   .catch((err)=>{
     res.status(404).send("해당 날짜에 데이터가 없습니다.")
